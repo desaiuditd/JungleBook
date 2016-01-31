8d1639021c69b7e7a4e153067fc850b4c155ba3d
Udit Desai <desaiuditd@gmail.com>
2016-01-30 16:25:29 -0800

    adds list view for animals

--- app/src/main/java/in/incognitech/junglebook/MainActivity.java
+++ app/src/main/java/in/incognitech/junglebook/MainActivity.java
@@ -3,8 +3,13 @@ package in.incognitech.junglebook;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import in.incognitech.junglebook.util.MenuController;
 
@@ -16,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String[] items = {"bat", "cat", "rat", "elephant", "panda", "dodo", "snake", "monkey", "cow", "buffalo", "lizzard", "man" };
+        ListView listView = (ListView) findViewById(R.id.list_view_animal);
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("COEN268", "Clicked position: " + position);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/in/incognitech/junglebook/model/Animal.java
@@ -0,0 +1,35 @@
+package in.incognitech.junglebook.model;
+
+/**
+ * Created by udit on 30/01/16.
+ */
+public class Animal {
+
+    private String name;
+    private String desc;
+    private String imageURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
